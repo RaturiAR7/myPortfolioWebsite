9f6aaa8f22839afa1e825bd626aa96f300de99f1
Anshul <anshulraturi007@gmail.com>
2024-10-10 18:09:32 +0530

    Fixed font color for the cards

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -15,16 +15,16 @@ const MyTimeline = () => {
           return (
             <VerticalTimelineElement
               className='vertical-timeline-element--work'
-              contentStyle={{ background: "black", color: "#fff" }}
+              contentStyle={{ background: "#D3D3D3", color: "#fff" }}
               contentArrowStyle={{ borderRight: "7px solid  white" }}
               date={element.date}
               iconStyle={{ background: "white", color: "black" }}
               icon={<img className='rounded-3xl w-56' src={element.icon} />}
             >
-              <h3 className='vertical-timeline-element-title text-white text-center text-3xl'>
+              <h3 className='vertical-timeline-element-title text-[#333333] text-center text-3xl font-extrabold'>
                 {element.title}
               </h3>
-              <h4 className='vertical-timeline-element-title text-white text-center text-3xl'>
+              <h4 className='vertical-timeline-element-title text-[#4A4A4A] text-center text-2xl font-semibold'>
                 {element.company_name}
               </h4>
               <p>{element.points}</p>
